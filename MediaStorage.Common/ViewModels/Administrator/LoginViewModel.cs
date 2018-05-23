752d40d0e8d5cba74f6f28eda30e5d3dd98d490d
muratkuru <muratkuru1995@gmail.com>
2018-05-23 06:11:04 +0300

    Add LoginViewModel

---
+++ MediaStorage.Common/ViewModels/Administrator/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaStorage.Common.ViewModels.Administrator
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Username", Prompt = "Username")]
+        [Required]
+        [MaxLength(30)]
+        public string Username { get; set; }
+
+        [Display(Name = "Password", Prompt = "Password")]
+        [DataType(DataType.Password)]
+        [Required]
+        [MaxLength(512)]
+        public string Password { get; set; }
+    }
+}

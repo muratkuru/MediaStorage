069d87f61ddcc06e4442c5a8a134edc7c429a59e
muratkuru <muratkuru1995@gmail.com>
2018-05-23 06:23:51 +0300

    Add DependencyConfig.

---
+++ MediaStorage.Web/App_Start/DependencyConfig.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using MediaStorage.Data;
+using MediaStorage.Service;
+using System.Reflection;
+
+namespace MediaStorage.Web.App_Start
+{
+    public static class DependencyConfig
+    {
+        public static AutofacDependencyResolver GetDependencyResolver()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MediaContext>().AsSelf().InstancePerRequest();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+
+            builder.RegisterType<PageService>().As<IPageService>();
+            builder.RegisterType<MaterialTypeService>().As<IMaterialTypeService>();
+            builder.RegisterType<AdministratorService>().As<IAdministratorService>();
+
+            IContainer container = builder.Build();
+
+            return new AutofacDependencyResolver(container);
+        }
+    }
+}
--- MediaStorage.Web/Global.asax.cs
+++ MediaStorage.Web/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using Autofac;
-using Autofac.Integration.Mvc;
-using MediaStorage.Data;
-using MediaStorage.Service;
-using MediaStorage.Web.App_Start;
-using System.Reflection;
+﻿using MediaStorage.Web.App_Start;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -17,26 +12,7 @@ namespace MediaStorage.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            DependencyResolver.SetResolver(GetDependencyResolver());
-        }
-
-        private AutofacDependencyResolver GetDependencyResolver()
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterType<MediaContext>().AsSelf().InstancePerRequest();
-
-            builder.RegisterControllers(Assembly.GetExecutingAssembly());
-
-            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
-            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
-
-            builder.RegisterType<PageService>().As<IPageService>();
-            builder.RegisterType<MaterialTypeService>().As<IMaterialTypeService>();
-
-            IContainer container = builder.Build();
-
-            return new AutofacDependencyResolver(container);
+            DependencyResolver.SetResolver(DependencyConfig.GetDependencyResolver());
         }
     }
 }

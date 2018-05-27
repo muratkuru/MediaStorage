53e273262b6dd142e0cc359c9b8fa5da0eefc261
muratkuru <muratkuru1995@gmail.com>
2018-05-27 18:46:05 +0300

    Add SelectListItem extension.

---
+++ MediaStorage.Web/Extensions/SelectListItemMapExtension.cs
@@ -0,0 +1,20 @@
+﻿using MediaStorage.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MediaStorage.Web
+{
+    public static class SelectListItemMapExtension
+    {
+        public static IEnumerable<SelectListItem> ToMVCSelectListItem(this ICollection<CustomSelectListItem> customSelectListItem)
+        {
+            return customSelectListItem.Select(s => new SelectListItem
+            {
+                Value = s.Value,
+                Text = s.Text,
+                Selected = s.Selected
+            });
+        }
+    }
+}

ad074deb914d704f2fd18c0e7e2d932c3b7bd726
muratkuru <muratkuru1995@gmail.com>
2018-06-01 18:51:46 +0300

    Add error pages.

---
+++ MediaStorage.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MediaStorage.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        public ActionResult ServerError()
+        {
+            return View();
+        }
+    }
+}
